c520b7ab68252e37ac4a3f2edcb77be7d41cd5d1
Borrero <juan.camilo.borrero.florez@gmail.com>
2026-09-24 23:45:43 -0500

    bugfix

--- server/src/main/java/co/icesi/buscaminas/controllers/TCPController.java
+++ server/src/main/java/co/icesi/buscaminas/controllers/TCPController.java
@@ -39,7 +39,7 @@ public class TCPController {
     public TCPController(ServicesImpl services, int port) {
         this.services = services;
         try {
-            serverSocket = new ServerSocket ( port , 50 , InetAddress . getByName (" 0.0.0.0 ") );
+            serverSocket = new ServerSocket (port,50,InetAddress.getByName ("0.0.0.0"));
 
             executor = Executors.newFixedThreadPool(5);
             gson = new GsonBuilder().create();
